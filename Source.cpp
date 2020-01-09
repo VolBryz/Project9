a3f1423aaee4dcb3a28d8802be6a82668fefe820
Bryzgalov Vladimir <v.bryzgalov2004@gmail.com>
2020-01-10 02:11:15 +0300

    Add new way to write Fibonacci numbers

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-
+/*
 int Fib(int i)
 
 {
@@ -10,10 +10,11 @@ int Fib(int i)
 	if (i == 1) return 1;
 	return Fib(i - 1) + Fib(i - 2);
 }
-
+*/
 int main()
 
 {
+/*
 	int i = 0;
 	while (i < 100)
 	{
@@ -21,6 +22,17 @@ int main()
 		i++;
 	}
 	return 0;
+
+*/
+	int i = 0;
+	int x = 2;
+	while (i < 100)
+	{
+		x = (x - 1) + (x - 2);
+			cout << x << endl;
+			i++;
+	}
+
 }
 
 
